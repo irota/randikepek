700bebe3906ed7538706d32637d575ae4ec76a8d
Robert Szabo <irotata@gmail.com>
2019-02-21 12:45:36 +0100

    Update sz_neked_silany_kiraly.js

--- sz_neked_silany_kiraly.js
+++ sz_neked_silany_kiraly.js
@@ -1,8 +1,7 @@
 javascript: (function () {
 
-	var newsrc=document.querySelector("form a img").src.replace("_process.php?filename=","/");
-		document.querySelector("form a img").src = newsrc;
-		newsrc=document.querySelector("form a img").src.replace("%2F","/");
-		document.querySelector("form a img").src = newsrc;
+	var newsrc = decodeURIComponent(document.querySelector("form a img").src).replace("_process.php?filename=","/");
+	
+	document.querySelector("form a img").src = newsrc;
 
 })();
