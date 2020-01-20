ab55f66193fabb6593856cfec4b18dd84a8a32ca
Robert Szabo <irotata@gmail.com>
2020-01-20 13:36:07 +0100

    Teljesen új verzió

--- bokmarklet.js
+++ bokmarklet.js
@@ -1,17 +1,14 @@
 javascript : (function () {
 	var head = document.getElementsByTagName("head")[0];
   
-  if(!document.getElementById("randibkmlt")){
+  if(!document.getElementById("randibkmlt" && !document.getElementById('bigstyle')){
     var bklScript = document.createElement("script");
     bklScript.type = "text/javascript";
     bklScript.id ="randibkmlt";
 
-    bklScript.src = "https://raw.githubusercontent.com/irota/randikepek/master/randikepek.min.js";
+    bklScript.src = "https://raw.githubusercontent.com/irota/randikepek/master/bigimages.min.js";
     head.appendChild(bklScript);
     
-  } else {
-     document.querySelector(".photooverlay").style = "display:flex !important;";
-    
-  }
+  } 
   
 })();
